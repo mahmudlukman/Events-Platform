f80974066cfb92be488e83a5041ed1e31afedd73
mahmud lukman <mistamann49@gmail.com>
2024-10-29 12:05:47 +0100

    Pagination fixed

--- client/components/shared/Pagination.tsx
+++ client/components/shared/Pagination.tsx
@@ -1,53 +1,53 @@
 "use client"
 
-import { formUrlQuery } from '@/lib/utils';
+import { useRouter, useSearchParams } from 'next/navigation'
+import React from 'react'
 import { Button } from '../ui/button'
-import { useRouter, useSearchParams } from 'next/navigation';
+import { formUrlQuery } from '@/lib/utils'
 
-interface Props {
-  pageNumber: number;
-  isNext: boolean;
+type PaginationProps = {
+  page: number | string,
+  totalPages: number,
+  urlParamName?: string
 }
 
-const Pagination = ({ pageNumber, isNext }: Props) => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
+const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
 
-  const handleNavigation = (direction: string) => {
-    const nextPageNumber = direction === 'prev'
-      ? pageNumber - 1
-      : pageNumber + 1;
+  const onClick = (btnType: string) => {
+    const pageValue = btnType === 'next' 
+      ? Number(page) + 1 
+      : Number(page) - 1
 
     const newUrl = formUrlQuery({
       params: searchParams.toString(),
-      key: 'page',
-      value: nextPageNumber.toString(),
+      key: urlParamName || 'page',
+      value: pageValue.toString(),
     })
 
-    router.push(newUrl)
+    router.push(newUrl, {scroll: false})
   }
 
-  if(!isNext && pageNumber === 1) return null;
-
   return (
-    <div className="flex w-full items-center justify-center gap-2">
+    <div className="flex gap-2">
       <Button
-        disabled={pageNumber === 1}
-        onClick={() => handleNavigation('prev')}
-        className="light-border-2 btn flex min-h-[36px] items-center justify-center gap-2 border"
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick('prev')}
+        disabled={Number(page) <= 1}
       >
-        <p className="body-medium text-dark200_light800">Prev</p>
+        Previous
       </Button>
-      <div className="flex items-center justify-center rounded-md bg-primary-500 px-3.5 py-2">
-        <p className="body-semibold text-light-900">{pageNumber}
-        </p>
-      </div>
       <Button
-        disabled={!isNext}
-        onClick={() => handleNavigation('next')}
-        className="light-border-2 btn flex min-h-[36px] items-center justify-center gap-2 border"
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick('next')}
+        disabled={Number(page) >= totalPages}
       >
-        <p className="body-medium text-dark200_light800">Next</p>
+        Next
       </Button>
     </div>
   )
