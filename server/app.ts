7e8cb5fa16cf1c20cb49b850739eeda5c0814adb
mahmud lukman <mistamann49@gmail.com>
2024-12-18 14:25:51 +0100

    test

--- server/app.ts
+++ server/app.ts
@@ -48,10 +48,10 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 //unknown route
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const err = new Error(`Route ${req.originalUrl} not found`) as any;
-  err.statusCode = 404;
-  next(err);
-});
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   const err = new Error(`Route ${req.originalUrl} not found`) as any;
+//   err.statusCode = 404;
+//   next(err);
+// });
 
 app.use(ErrorMiddleware);
